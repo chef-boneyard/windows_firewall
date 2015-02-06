a1d65c71304138666c7b2dc6b8cea7833a2ab8f9
Matt Stratton <matt.stratton@gmail.com>
2015-02-05 22:43:56 -0600

    Update license to Apache 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'windows_firewall'
 maintainer       'Matt Clifton'
 maintainer_email 'spartacus003@hotmail.com'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Configures firewall rules on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 #source_url       'https://github.com/lynx44/windows_firewall'
