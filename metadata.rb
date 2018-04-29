b6bfa42b6a5e1d78f6c4ac58e476ba32268137bf
Tim Smith <tsmith@chef.io>
2018-04-29 16:51:03 -0700

    Add source/issue_url metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'spartacus003@hotmail.com'
 license          'Apache 2.0'
 description      'Configures firewall rules on Windows'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-#source_url       'https://github.com/lynx44/windows_firewall'
+source_url       'https://github.com/schubergphilis/windows_firewall'
+issues_url       'https://github.com/schubergphilis/windows_firewall/issues'
 supports         'windows'
 version          '3.0.2'
 chef_version '>= 12.7' if respond_to?(:chef_version)
